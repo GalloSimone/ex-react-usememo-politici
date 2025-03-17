187550dcf5659963dac9afc619d5f691dc2e0c34
GalloSimone <gallosimonecz@gmail.com>
2025-03-17 11:17:06 +0100

    milestone 2 completata

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,30 @@ import './App.css'
 
 function App() {
   const [politicians, setPoliticians] = useState([])
+const [searchTerm, setSearchTerm]=useState("")
   useEffect(()=>{
     fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
     .then((res)=>res.json())
     .then((data)=>setPoliticians(data))
     .catch((error)=>console.error("errore"))
   },[]);
+  const filteredPoliticians=politicians.filter((p)=>
+  p.name.toLowerCase().includes(searchTerm.toLowerCase())||
+  p.biography.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <>
      <div>
       <h1>lista politici</h1>
+      <input
+       type="text" 
+       placeholder='cerca'
+       value={searchTerm}
+       onChange={(e)=>setSearchTerm(e.target.value)}
+      />
       <ul>
-        {politicians.map((politician)=>(
+        {filteredPoliticians.map((politician)=>(
           <li key={politician.id}>
            <p>nome:{politician.name}</p>
            <p>immagine:{politician.image}</p>
